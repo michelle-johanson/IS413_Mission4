d9aa2db7a78d647128e33353aef1b3514b56cd05
michelle-johanson <mjohans0@byu.edu>
2026-01-28 10:38:59 -0700

    merge w/ Sabrina

--- Program.cs
+++ Program.cs
@@ -6,23 +6,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Game Starts
-        
-        // Player X places X in row 1, col 1
-        UpdateBoard('X', 1, 1);
-        
-        char[][] boardArray =
+        TicTacBoard ttb = new TicTacBoard();
+        char[][] board =
         {
             new char[3],
             new char[3],
             new char[3]
         };
 
-    }
+        //Welcome user to the game 
+        Console.WriteLine("Welcome to the Tic-Tac-Toe Game!");
+        Console.WriteLine("Player 1 = X, Player 2 = O\n");
 
-    public void UpdateBoard(char player, int row, int col, char[][] boardArray)
-    {
-        boardArray[row][col] = player;
-        TicTacBoard board = new TicTacBoard(boardArray);
+        // player one starts the game
+        char currentPlayer = 'X';
+        bool gameWon = false;
+
+        while (!gameWon)
+        {
+            Console.Write($"Player {(currentPlayer == 'X' ? "1" : "2")}: \nChoose a row (1-3): ");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write($"Choose a column (1-3): ");
+            int col = int.Parse(Console.ReadLine());
+
+            board[row][col] = currentPlayer;
+            ttb.PrintBoard();
+        }
+
     }
 }
